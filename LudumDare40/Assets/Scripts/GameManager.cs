dafb314c9c71560ead1a33f4566d24c08bcafe40
seanocca <32288543+seanocca@users.noreply.github.com>
2017-12-02 14:08:21 +1000

    PlayerMovement starting to work
    PlayerMovement isn't fluid but it has bare bones of forward movement.
    
    Nothing side to side or user controlled yet

--- LudumDare40/Assets/Scripts/GameManager.cs
+++ LudumDare40/Assets/Scripts/GameManager.cs
@@ -13,4 +13,14 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void PlayGame()
+    {
+        //Setup Player
+            //GameObject player = Instantiate(playerPrefab, new Vector3(Maze.GetPlayerStartingX(), 0.5f, Maze.GetPlayerStartingZ()), Quaternion.identity);
+
+        //Setup Camera and other game neccessities
+            //gameCamera.transform.position = new Vector3();
+            //menu.SetActive(false);
+    }
 }
--- LudumDare40/Assets/Scripts/PlayerMovement.cs
+++ LudumDare40/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour {
     public static Vector3 currPlayerPosition;
 
     public float pharmAmount = 100f;
+    private float playerSpeed = 0.5f;
+
+    private int speedLoop;
 
     private bool isMoving = false;
-
 
 	// Use this for initialization
 	void Start () {
@@ -18,18 +20,52 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        StartCoroutine("Moving");
+
+
+        if (isMoving == false)
+        {
+            Debug.Log("Player Coroutine ON");
+            StartCoroutine("PlayerMove");
+            //StartCoroutine("HitDetection");
+        }else if (isMoving == false && pharmAmount <= 0f)
+        {
+            StopCoroutine("PlayerMove");
+        }
 	}
 
     IEnumerator PlayerMove()
     {
         isMoving = true;
-        while (pharmAmount <= 0f)
+        while (pharmAmount > 0f)
         {
-            currPlayerPosition.z += 1f;
-            this.transform.position.z = currPlayerPosition;
+            Debug.Log("Player is Moving");
+            currPlayerPosition = this.transform.position;
+            if (playerSpeed == 0.25f)
+            {
+                speedLoop++;
+                if (speedLoop == 3)
+                {
+                    playerSpeed = 0.5f;
+                }
+            }
+            currPlayerPosition.z += playerSpeed;
+            this.transform.position = currPlayerPosition;
+            yield return new WaitForSecondsRealtime(0.5f);
         }
         isMoving = false;
         yield return null;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DeathObstacle")
+        {
+            StopCoroutine("Moving");
+        }else if (other.gameObject.tag == "SlowObstacle")
+        {
+            //Slow player down
+            playerSpeed = 0.25f;
+        }
+    }
+
 }
